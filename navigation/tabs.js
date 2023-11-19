a336b72cee31f920ca4b64e151b0f3b4cafbc656
prince1107 <princeofwakanda0607@gmail.com>
2023-11-19 11:41:58 -0600

    added log in to tabs

--- navigation/tabs.js
+++ navigation/tabs.js
@@ -5,6 +5,7 @@ import AddStockScreen from '../screens/AddStockScreen';
 import NewsScreen from '../screens/NewsScreen';
 import PortfolioScreen from '../screens/PortfolioScreen';
 import SettingScreen from '../screens/SettingScreen';
+import LoginScreen from '../screens/LoginScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -15,6 +16,7 @@ const Tabs = () => {
             <Tab.Screen name="News" component={NewsScreen} />
             <Tab.Screen name="Portfolio" component={PortfolioScreen} />
             <Tab.Screen name="Settings" component={SettingScreen} />
+            <Tab.Screen name="Log In" component={LoginScreen} />
         </Tab.Navigator>
     );
 }
